6afe7efdeef0a43dc15fdcdec1c44d18220afadd
Daniel Yim <daniel.yim@getcruise.com>
2018-05-18 14:54:54 -0700

    adding screenshot defaults

--- macos-settings.sh
+++ macos-settings.sh
@@ -2,3 +2,8 @@
 
 # Disable displaying the accents panel when holding a key (so easier key repeats)
 defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Change the default screenshot location to ~/Screenshots
+mkdir -p ~/Screenshots
+defaults write com.apple.screencapture location
+defaults write com.apple.screencapture name "img"
