4ebbbf6c2dff9f0eae9358c30c5a4bdc07d31741
Daniel Yim <danielyim@gmail.com>
2018-05-28 22:13:49 -0700

    updating scripts

--- install.sh
+++ install.sh
@@ -1,11 +1,29 @@
 #!/bin/sh
 
-cd $HOME
+current_dir="$( cd "$( dirname "${bash_source[0]}" )" && pwd )"
+source "$current_dir/lib/helpers.sh"
+
+INSTALL_ROOT=$HOME
 
 # Install antigen (oh-my-zsh plugin manager)
+echo "Installing antigen..."
 sudo mkdir -p /usr/local/share/antigen
 sudo sh -c "curl -L git.io/antigen > /usr/share/antigen/antigen.zsh"
 
 # Install oh-my-zsh
+echo "Installing oh-my-zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
+# Install z
+echo "Installing z (jump around)..."
+git clone  --depth 1 https://github.com/rupa/z ~/z
+
+# Install fzf
+echo "Installing fzf..."
+git clone  --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+# Install vim-plug
+echo "Installing vim-plug..."
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
--- lib/helpers.sh
+++ lib/helpers.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# Checks if the first argument exists, if so, copy to the 2nd arg (backup directory)
 backup_if_exists() {
   [[ -e $1 ]] && cp $1 $2 
 }
 
+# Checks if the current operating system is macOS
 is_macos() {
   local platform=$(uname)
   [ "$platform" == "Darwin" ]
+}
+
+# Checks if a command exists (in PATH and executable)
+command_exists() {
+  [ -x "$(command -v $1)" ]
 }
