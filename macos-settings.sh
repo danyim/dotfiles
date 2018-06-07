eb79f3c00e01e331f22a84de690e5001e4d941d9
Daniel Yim <daniel.yim@getcruise.com>
2018-06-07 09:49:37 -0700

    update mac settings

--- macos-settings.sh
+++ macos-settings.sh
@@ -3,7 +3,11 @@
 # Disable displaying the accents panel when holding a key (so easier key repeats)
 defaults write -g ApplePressAndHoldEnabled -bool false
 
+# Disable special characters when holding down alt 
+defaults write -g NSUserKeyEquivalents -dict-add "Special Characters..." nul
+
 # Change the default screenshot location to ~/Screenshots
-mkdir -p ~/Screenshots
-defaults write com.apple.screencapture location
+SCREENSHOT_DIR=$HOME/Screenshots
+mkdir -p $SCREENSHOT_DIR
+defaults write com.apple.screencapture location $SCREENSHOT_DIR
 defaults write com.apple.screencapture name "img"
