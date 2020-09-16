3b391bb410e4cfb933cda149cb902f37356e7a4e
Daniel Yim <danielyim@gmail.com>
2020-09-16 16:10:07 -0700

    Updating os-specific kubectl install

--- install.sh
+++ install.sh
@@ -67,7 +67,14 @@ git clone --depth 1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # Install kubectl
 echo "Installing kubectl..."
-curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt` /bin/linux/amd64/kubectl
+if is_macos; then
+  curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/darwin/amd64/kubectl"
+else
+  curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt` /bin/linux/amd64/kubectl
+fi
+
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
 
 if is_macos; then
   echo "Installing Homebrew..."
