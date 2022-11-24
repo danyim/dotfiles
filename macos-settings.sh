c5abd3dfe00136e1f948b059804c94334512e181
Daniel Yim <danielyim@gmail.com>
2022-11-23 21:58:04 -0800

    Add font smoothing

--- macos-settings.sh
+++ macos-settings.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Use font smoothing https://github.com/alacritty/alacritty/issues/4616#issuecomment-1236413444
+defaults write -g AppleFontSmoothing -int 0
+
 # Disable displaying the accents panel when holding a key (so easier key repeats)
 defaults write -g ApplePressAndHoldEnabled -bool false
 
