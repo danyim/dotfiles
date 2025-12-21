86b6c193129dc1e8051ac78bb23b861a79d7f2d3
Daniel Yim <danielyim@gmail.com>
2025-12-21 14:46:09 -0800

    LLM audit and fixes

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
-current_dir="$( cd "$( dirname "${bash_source[0]}" )" && pwd )"
+current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$current_dir/lib/helpers.sh"
 
-INSTALL_ROOT=$HOME/tmp
+INSTALL_ROOT="$HOME/tmp"
 
-mkdir -p $INSTALL_ROOT
+mkdir -p "$INSTALL_ROOT"
 
 # Elevate shell script
 [ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
@@ -26,8 +26,8 @@ sudo sh -c "curl -L git.io/antigen > /usr/local/share/antigen/antigen.zsh"
 # Install z
 echo "Installing z (jump around)..."
 sudo mkdir -p /etc/profile.d
-git clone --depth 1 https://github.com/rupa/z $INSTALL_ROOT/z
-sudo cp $INSTALL_ROOT/z/z.sh /etc/profile.d
+git clone --depth 1 https://github.com/rupa/z "$INSTALL_ROOT/z"
+sudo cp "$INSTALL_ROOT/z/z.sh" /etc/profile.d
 
 # Install fzf
 echo "Installing fzf..."
@@ -36,8 +36,8 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 
 # Install diff-so-fancy
 echo "Installing diff-so-fancy..."
-git clone git@github.com:so-fancy/diff-so-fancy.git $INSTALL_ROOT/diff-so-fancy
-sudo ln -s $INSTALL_ROOT/diff-so-fancy/diff-so-fancy /usr/local/bin/diff-so-fancy
+git clone https://github.com/so-fancy/diff-so-fancy.git "$INSTALL_ROOT/diff-so-fancy"
+sudo ln -s "$INSTALL_ROOT/diff-so-fancy/diff-so-fancy" /usr/local/bin/diff-so-fancy
 
 # Install vim-plug
 echo "Installing vim-plug..."
@@ -50,7 +50,7 @@ curl https://sh.rustup.rs -sSf | sh
 
 # Install Alacritty
 echo "Installing Alacritty..."
-git clone --depth 1 https://github.com/jwilm/alacritty $INSTALL_ROOT/alacritty
+git clone --depth 1 https://github.com/jwilm/alacritty "$INSTALL_ROOT/alacritty"
 
 # Install broot
 echo "Installing broot..."
@@ -62,7 +62,7 @@ cargo install exa
 
 # Install nvm
 echo "Installing nvm..."
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
 
 # Install TPM
 echo "Installing TPM (tmux plugin manager)..."
@@ -72,20 +72,31 @@ git clone --depth 1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 echo "Installing kubectl..."
 if is_macos; then
   curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/darwin/amd64/kubectl"
+elif is_linux; then
+  curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
 else
-  curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt` /bin/linux/amd64/kubectl
+  echo "Unsupported platform for kubectl installation"
+  exit 1
 fi
 
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
 if is_macos; then
-  echo "Installing Homebrew..."
-  mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
+  if ! is_command_available brew; then
+    echo "Installing Homebrew..."
+    mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
+  else
+    echo "Homebrew already installed, skipping..."
+  fi
+  
   brew install \
     git vim tmux jq hub ripgrep
-  brew cask install \
+  brew install --cask \
     spectacle clipy alfred spotify
 fi
 
-echo "\n\nComplete. Please open a new shell."
+printf "\n\nComplete. Please open a new shell.\n"
+
+# Cleanup temporary install directory
+rm -rf "$INSTALL_ROOT"
--- lib/helpers.sh
+++ lib/helpers.sh
@@ -2,16 +2,40 @@
 
 # Checks if the first argument exists, if so, copy to the 2nd arg (backup directory)
 backup_if_exists() {
-  [[ -e "$1" ]] && cp "$1" "$2" 
+  if [[ -e "$1" ]] && [[ -r "$1" ]]; then
+    local dest_dir
+    dest_dir="$(dirname "$2")"
+    if [[ -d "$dest_dir" ]] && [[ -w "$dest_dir" ]]; then
+      cp "$1" "$2"
+      return $?
+    else
+      return 1
+    fi
+  else
+    return 1
+  fi
 }
 
 # Checks if the current operating system is macOS
 is_macos() {
-  local platform=$(uname)
-  [ "$platform" == "Darwin" ]
+  local platform
+  platform="$(uname)"
+  [ "$platform" = "Darwin" ]
+}
+
+# Checks if the current operating system is Linux
+is_linux() {
+  local platform
+  platform="$(uname)"
+  [ "$platform" = "Linux" ]
 }
 
 # Checks if a command exists (in PATH and executable)
 command_exists() {
-  [ -x "$(command -v $1)" ]
+  [ -x "$(command -v "$1")" ]
+}
+
+# Checks if a command is available (alternative name for command_exists)
+is_command_available() {
+  command_exists "$1"
 }
